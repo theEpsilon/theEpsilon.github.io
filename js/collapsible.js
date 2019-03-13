ab3aed6055627d3d9972b409d96edfa5a64f27b0
unknown <f.berndt99@gmail.com>
2019-03-14 00:58:49 +0100

    Rebuilt collapsibles to work w/o JS

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   equalizeHeights();
-  //assignClickEvents();
+  assignClickEvents();
 
   var iv = null;
 
@@ -17,37 +17,49 @@ $(document).ready(function() {
 /*
 Called upon page loadup, assigning click listeners to collapsible items
 */
-/*
+
 function assignClickEvents() {
 
-  var collapsibles = new Array();
-
   $(".js-coll").each(function() {
-    collapsibles.push($(this));
 
-    $(this).find(".lbl-toggle").click(function() {
+    $(this).find(".toggle").click(function() {
 
-      var current = $(this).parent();
-      var content = current.find(".collapsible-content");
+      var correspLabel = $("label[for='"+$(this).attr("id")+"']");
 
-      if(content.css("max-height") != "0px") {
-        content.css("max-height", "0px");
-        current.find(".dropdown-icon").css({transform: "rotate(0deg)", transition: "0.2s"});
-
-        content.on("transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd", function() {
-          equalizeHeights();
-          content.off();
-        });
-
+      if ($(this).is(":checked")) {
+        correspLabel.find(".dropdown-icon").css({
+          '-webkit-transform': "rotate(180deg)",
+          '-moz-transform': "rotate(180deg)",
+          '-ms-transform': "rotate(180deg)",
+          '-o-transform': "rotate(180deg)", 
+          'transform': "rotate(180deg)",
+          '-webkit-transition': "0.2s",
+          '-moz-transition': "0.2s",
+          '-ms-transition': "0.2s",
+          '-o-transition': "0.2s",
+          'transition': "0.2s"});
       } else {
-        content.css("max-height", content.prop("scrollHeight") + 200 +"px");
-        current.css("height", "auto");
-        current.find(".dropdown-icon").css({transform: "rotate(180deg)", transition: "0.2s"});
+        correspLabel.find(".dropdown-icon").css({
+          '-webkit-transform': "rotate(0deg)",
+          '-moz-transform': "rotate(0deg)",
+          '-ms-transform': "rotate(0deg)",
+          '-o-transform': "rotate(0deg)", 
+          'transform': "rotate(0deg)",
+          '-webkit-transition': "0.2s",
+          '-moz-transition': "0.2s",
+          '-ms-transition': "0.2s",
+          '-o-transition': "0.2s",
+          'transition': "0.2s",
+          '-webkit-transition-delay': "0.1s",
+          '-moz-transition-delay': "0.1s",
+          '-ms-transition-delay': "0.1s",
+          '-o-transition-delay': "0.1s",
+          'transition-delay': "0.1s"});
       }
     });
   });
 }
-*/
+
 function equalizeHeights() {
   var heights = new Array();
 
@@ -63,43 +75,12 @@ function equalizeHeights() {
 
   var maxHeight = Math.max.apply(Math, heights);
 
-  console.log(maxHeight);
-
   $(".banner-header").each(function() {
 
   	var outerheight = $(this).outerHeight(true);
-
-  	console.log(outerheight);
-  	console.log(maxHeight > outerheight);
 
   	if(maxHeight > outerheight) {
   		$(this).find(".text").css("margin-bottom", (maxHeight - outerheight) + "px");
   	}
   });
-  /*
-  $(".js-coll").each(function() {
-
-    console.log($(this));
-
-    if($(this).find(".collapsible-content").css("max-height") == "0px") {
-      $(this).css("height", "auto");
-      $(this).find(".text").css("margin-bottom", "0px");
-
-      heights.push($(this).outerHeight(true));
-    }
-  });
-	
-  var maxHeight = Math.max.apply(Math, heights);
-
-  console.log(maxHeight);
-
-  $(".js-coll").each(function() {
-    if($(this).find(".collapsible-content").css("max-height") == "0px") {
-      var outerheight = $(this).outerHeight(true);
-
-      if(maxHeight > outerheight) {
-        $(this).find(".text").css("margin-bottom", (maxHeight - outerheight) + "px");
-      }
-    }
-  });*/
 }
